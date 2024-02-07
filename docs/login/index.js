193e35c55d2ffe8623cb57f309fd1965de9b4cf5
Igor Novozhilov <maildrakon@gmail.com>
2024-02-07 10:21:28 +0300

    login

--- docs/login/index.js
+++ docs/login/index.js
@@ -6,8 +6,12 @@ async function checkLogin() {
   const token = window.location.pathname === '/login/' ? new URLSearchParams(window.location.search).get('token') : null
   const data = await apiFetch('check-login', { token })
 
-  if (data.login) { // @ts-ignore
-    window.location = '/'
+  if (data.login) {
+    const ret = new URLSearchParams(window.location.search).get('ret') || '/'
+    const url = new URL(window.location.origin)
+
+    url.pathname = ret // @ts-ignore
+    window.location = url.href
   } else {
     await showLoginForm(data.token)
     checkLogin()
